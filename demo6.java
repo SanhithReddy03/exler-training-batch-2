e0c03d3753e5edfff882b4b0f23ea57c95bd2dc4
SanhithReddy03 <2311CS010691@mallareddyuniversity.ac.in>
2025-01-03 10:17:23 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,10 @@
+public class demo6{
+ public static void main(String[] args) {
+    String username = "sunny";
+    String pwd = "sunny@123";
+    while((username == "sunny") && (pwd == "sunny@123")){
+    System.out.println("hello sunny");
+    break;
+    }
+ }
+}
