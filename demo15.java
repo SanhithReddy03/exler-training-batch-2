46bac9cbb1aad4197659d3e96260843acd8ed2c6
SanhithReddy03 <2311CS010691@mallareddyuniversity.ac.in>
2025-01-07 10:18:09 +0530

    swapping of two numbers

---
+++ demo15.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println("a = " + a + " b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a + " b = " + b);
+        
+        
+    }
+}
