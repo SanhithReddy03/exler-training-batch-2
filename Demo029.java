d0714b539fe5cbca02847c50f0f5f2a577b50f2e
SanhithReddy03 <2311CS010691@mallareddyuniversity.ac.in>
2025-01-20 09:52:57 +0530

    arrays example

---
+++ Demo029.java
@@ -0,0 +1,20 @@
+public class Demo029 {
+    public static void main(String[] args) {
+                String[] names = {
+            "sunny",
+            "gowtham",
+            "Charlie",
+            "revan",
+            "sriram",
+            "ram",
+            "adithya",
+            "pavan",
+            "nikhil",
+            
+        };
+        System.out.println("The names in the array are:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
