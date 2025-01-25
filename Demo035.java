61a03a83182c0465ba357f9f13b79916fc2cd96e
SanhithReddy03 <2311CS010691@mallareddyuniversity.ac.in>
2025-01-25 10:28:52 +0530

    Add files via upload

---
+++ Demo035.java
@@ -0,0 +1,13 @@
+public class Demo035{
+    public static void main(String[] args){
+        int a,b;
+        a = 20;
+        b = 30;
+        if(a > b){
+            System.out.println("a is greater");
+        }
+        else {
+            System.out.println("b is greater");
+        }
+    }
+}
